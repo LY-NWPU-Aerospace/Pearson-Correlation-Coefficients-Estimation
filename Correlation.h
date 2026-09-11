18cf41019ac7d3b57fc32304b1297f764dac4b23
LY-NWPU-Aerospace <cvfour@outlook.com>
2026-09-11 11:35:06 +0800

    Add files via upload

--- Correlation.h
+++ Correlation.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <set>
+#include <string>
+
 #define DO_NOT_EXIST_VALUE -801.403
 
 #ifdef DLL_EXPORT
@@ -15,65 +17,48 @@ int eliminateLinearVariables(std::vector<std::vector<double>> in,
 	std::vector<std::vector<double>>& out, std::vector<int>& outIndex,
 	std::vector<int>& eliminatedIndex, double tolerance = 1e-3);
 
-struct DikinEllipsoid
+class DomainCenter
 {
 public:
-	std::vector<std::vector<double>> H;			// Hessian matrix
-	std::vector<std::vector<double>> H_i;		// Inverse of H
-	std::vector<std::vector<double>> eigVec;	// Eigen vectors, row vector
-	std::vector<double> x_center;				// The center of the ellipsoid
-	std::vector<double> x_lower;				// The lower bounds of the ellipsoid
-	std::vector<double> x_upper;				// The upper bounds of the ellipsoid
-	std::vector<double> length;					// The axial length of the ellipsoid
-};
-
-struct DikinCenter
-{
 	bool success;
 	int num_iters;
-	int opt_type;
 	double final_obj;
 	double time_cost;
-	std::vector<double> obj;
-	std::vector<std::vector<double>> corr;
-	std::vector<double> var;
-
-	std::vector<double> eigValue;
 	double condNum;
 	double minEigValue;
 	double determinant;
-
+	std::vector<double> obj;
+	std::vector<std::vector<double>> corr;
+	std::vector<double> var;
+	std::vector<double> eigValue;
+	std::vector<std::vector<double>> eigVector;
 };
 
-struct ErrorBenefit
+struct IdentifiedResult
 {
-	double maxErrorR_Euclidean;
-	double ZaBoundsR_Euclidean;
-	double maxError_Riemannian;
-	double ZaBounds_Riemannian;
-	double minBenefit;
-	double maxBeneift;
+	std::vector<double> indenfied_d;
+	std::vector<int> indenfied_index;
+
+	std::vector<std::vector<int>> indenfied_factor_Matrix;
+	std::vector<std::vector<double>> indenfied_d_Matrix;
+};
+
+struct Metrics
+{
 	double r_b;
-};
-
-struct ErrorImprove
-{
-	double supError;
-	double improvement;
-	std::vector<double> MaxCorrelated;
-	std::vector<double> MinCorrelated;
+	double eps_E_t;
+	double d_r_b;
+	double d_eps_E_t;
+	std::vector<double> direction_eps;
+	std::vector<double> direction_rb;
 };
 
 struct CorrResult
 {
-	DikinCenter chebyshev;
-	DikinCenter analytic;
-	DikinEllipsoid dikin_base;
-	DikinEllipsoid dikin_inflation;
-	ErrorBenefit paras;
-
-	std::vector<std::vector<double>> corr;
-	std::vector<std::vector<double>> longest;
+	DomainCenter chebyshev;
+	DomainCenter analytic;
+	Metrics paras;
+	IdentifiedResult identRes;
 };
 
 struct MarginalDisInfo
@@ -84,6 +69,15 @@ struct MarginalDisInfo
 	std::vector<double> cdf;
 };
 
+struct OptOption
+{
+	bool active = false;				// use opt or not
+	int numThreads = 1;					// number of threads for opt DE algorithm
+	int popSize = 1e3;					// population size
+	int maxIterations = 1e3;			// maximum number of iterations
+	std::string saveDir = "\\optRes";	// saved path
+};
+
 class DLL_API Correlation_API
 {
 public:
@@ -92,19 +86,9 @@ public:
 	// set the corr matrix
 	bool setCorrlationMatrix(std::vector<std::vector<double>> corr, Correlation* curCoef = nullptr);
 
-	// set the tolerance of the convergence
 	void setTolerance(double _value);
 
-	// estimate the bounds of the unknown elements of the matrix
-	CorrResult runEstimation();
-
-	std::vector<std::vector<double>> getBoundryMatrix(std::vector<double> direction);
-
-	double getBoundryLength(std::vector<double> direction);
-
-	double getBoundryLength(std::set<int> index);
-
-	ErrorImprove getImproveIndex(std::set<int> variableIndex);
+	CorrResult runEstimation(std::set<int> variableIndex, OptOption _option);
 
 protected:
 
